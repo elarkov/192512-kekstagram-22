e7e27a0fc0ebb6db8d0e7f749acff539d124777e
Evgeniy Larkov <elarkov@inbox.ru>
2021-04-02 09:54:52 +0700

    Добавил в точку входа модуль загрузки изображения

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 import { isLengthString } from './util.js';
 import { generateCommentMessage } from './comment.js';
 import { showPhoto } from './thumbnails.js';
+import { uploadPicture } from './upload-file.js';
 
 showPhoto();
 isLengthString(generateCommentMessage().message);
+uploadPicture();
+
+
 
 
 
