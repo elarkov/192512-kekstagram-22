29d5d0d0dfb91ef1f696337b4f32ce0b18b68519
Evgeniy Larkov <elarkov@inbox.ru>
2021-04-04 19:05:40 +0700

    Добавил слайдер для эффектов

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
+
 import { isLengthString } from './util.js';
 import { generateCommentMessage } from './comment.js';
 import { showPhoto } from './thumbnails.js';
-import { uploadPicture, scaleImage, changeEffectFilter } from './upload-file.js';
+import { uploadPicture, changeScaleImage, changeEffectFilter } from './upload-file.js';
 
 showPhoto();
 isLengthString(generateCommentMessage().message);
 uploadPicture();
-scaleImage();
+changeScaleImage();
 changeEffectFilter();
 
 
--- js/upload-file.js
+++ js/upload-file.js
@@ -12,6 +12,51 @@ const scaleValueControl = scaleContainer.querySelector('.scale__control--value')
 const scaleSmallerButton = scaleContainer.querySelector('.scale__control--smaller');
 const scaleBiggerButton = scaleContainer.querySelector('.scale__control--bigger');
 const effectElementRadio = uploadForm.querySelectorAll('.effects__preview');
+const sliderElement = uploadForm.querySelector('.effect-level__slider');
+const effectSliderValue = uploadForm.querySelector('.effect-level__value');
+
+const MIN_FILTER_VALUE = 0;
+const MAX_FILTER_VALUE = 100;
+const MIN_START_VALUE = 100;
+const STEP_VALUE_EFFECT = 1;
+const MIN_FILTER_VALUE_CHROME = 0;
+const MAX_FILTER_VALUE_CHROME = 1;
+const MIN_START_VALUE_CHROME = 1;
+const STEP_VALUE_EFFECT_CHROME = 0.1;
+const MIN_FILTER_VALUE_MARVIN = 0;
+const MAX_FILTER_VALUE_MARVIN = 100;
+const MIN_START_VALUE_MARVIN = 100;
+const STEP_VALUE_EFFECT_MARVIN = 1;
+const MIN_FILTER_VALUE_PHOBOS = 0;
+const MAX_FILTER_VALUE_PHOBOS = 3;
+const MIN_START_VALUE_PHOBOS = 3;
+const STEP_VALUE_EFFECT_PHOBOS = 0.1;
+const MIN_FILTER_VALUE_HEAT = 1;
+const MAX_FILTER_VALUE_HEAT = 3;
+const MIN_START_VALUE_HEAT = 3;
+const STEP_VALUE_EFFECT_HEAT = 0.1;
+
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: MIN_FILTER_VALUE,
+    max: MAX_FILTER_VALUE,
+  },
+  start: MIN_START_VALUE,
+  step: STEP_VALUE_EFFECT,
+  connect: 'lower',
+  format: {
+    to: function (value) {
+      if (Number.isInteger(value)) {
+        return value.toFixed(0);
+      }
+      return value.toFixed(1);
+    },
+    from: function (value) {
+      return parseFloat(value);
+    },
+  },
+});
 
 const uploadPicture = () => {
 
@@ -37,7 +82,7 @@ const uploadPicture = () => {
   document.addEventListener('keydown', closeKey);
 };
 
-const scaleImage = () => {
+const changeScaleImage = () => {
   const checkButton = () => {
     if (parseInt(scaleValueControl.value) === MIN_VALUE) {
       scaleSmallerButton.disabled = true;
@@ -67,38 +112,95 @@ const scaleImage = () => {
 };
 
 const changeEffectFilter = () => {
+  sliderElement.style.display = 'none';
+
   for (let i = 0; i < effectElementRadio.length; i++) {
     effectElementRadio[i].addEventListener('click', () => {
       imgPreview.firstElementChild.className = '';
-
-      switch (effectElementRadio[i].classList[1]) {
-        case 'effects__preview--none': {
-          imgPreview.style.filter = 'none';
-          break;
-        }
-        case 'effects__preview--chrome': {
-          imgPreview.style.filter = 'grayscale(1)';
-          break;
-        }
-        case 'effects__preview--sepia': {
-          imgPreview.style.filter = 'sepia(1)';
-          break;
-        }
-        case 'effects__preview--marvin': {
-          imgPreview.style.filter = 'invert(100%)';
-          break;
-        }
-        case 'effects__preview--phobos': {
-          imgPreview.style.filter = 'blur(3px)';
-          break;
-        }
-        case 'effects__preview--heat': {
-          imgPreview.style.filter = 'brightness(3)';
-          break;
-        }
+      imgPreview.firstElementChild.classList.add(String(effectElementRadio[i].classList[1]));
+      if (effectElementRadio[i].classList[1] == 'effects__preview--none') {
+        sliderElement.style.display = 'none';
+        sliderElement.noUiSlider.updateOptions({
+          range: {
+            min: MIN_FILTER_VALUE
+            max: MAX_FILTER_VALUE,
+          },
+          start: MIN_START_VALUE,
+          step: STEP_VALUE_EFFECT,
+        });
+      } else if (effectElementRadio[i].classList[1] == 'effects__preview--chrome' || effectElementRadio[i].classList[1] == 'effects__preview--sepia') {
+        sliderElement.style.display = 'block';
+        sliderElement.noUiSlider.updateOptions({
+          range: {
+            min: MIN_FILTER_VALUE_CHROME,
+            max: MAX_FILTER_VALUE_CHROME,
+          },
+          start: MIN_START_VALUE_CHROME,
+          step: STEP_VALUE_EFFECT_CHROME,
+        });
+      } else if (effectElementRadio[i].classList[1] == 'effects__preview--marvin') {
+        sliderElement.style.display = 'block';
+        sliderElement.noUiSlider.updateOptions({
+          range: {
+            min: MIN_FILTER_VALUE_MARVIN,
+            max: MAX_FILTER_VALUE_MARVIN,
+          },
+          start: MIN_START_VALUE_MARVIN,
+          step: STEP_VALUE_EFFECT_MARVIN,
+        });
+      } else if (effectElementRadio[i].classList[1] == 'effects__preview--phobos') {
+        sliderElement.style.display = 'block';
+        sliderElement.noUiSlider.updateOptions({
+          range: {
+            min: MIN_FILTER_VALUE_PHOBOS,
+            max: MAX_FILTER_VALUE_PHOBOS,
+          },
+          start: MIN_START_VALUE_PHOBOS,
+          step: STEP_VALUE_EFFECT_PHOBOS,
+        });
+      } else if (effectElementRadio[i].classList[1] == 'effects__preview--heat') {
+        sliderElement.style.display = 'block';
+        sliderElement.noUiSlider.updateOptions({
+          range: {
+            min: MIN_FILTER_VALUE_HEAT,
+            max: MAX_FILTER_VALUE_HEAT,
+          },
+          start: MIN_START_VALUE_HEAT,
+          step: STEP_VALUE_EFFECT_HEAT,
+        });
       }
+
+      sliderElement.noUiSlider.on('update', (value, handle) => {
+        effectSliderValue .setAttribute('value', value[handle]);
+        switch (effectElementRadio[i].classList[1]) {
+          case 'effects__preview--none': {
+            imgPreview.style.filter = 'none';
+            break;
+          }
+          case 'effects__preview--chrome': {
+            imgPreview.style.filter = 'grayscale(1)';
+            break;
+          }
+          case 'effects__preview--sepia': {
+            imgPreview.style.filter = 'sepia(1)';
+            break;
+          }
+          case 'effects__preview--marvin': {
+            imgPreview.style.filter = 'invert(100%)';
+            break;
+          }
+          case 'effects__preview--phobos': {
+            imgPreview.style.filter = 'blur(3px)';
+            break;
+          }
+          case 'effects__preview--heat': {
+            imgPreview.style.filter = 'brightness(3)';
+            break;
+          }
+        }
+      });
     });
   }
 };
 
-export { uploadPicture, scaleImage, changeEffectFilter };
+export { uploadPicture, changeScaleImage, changeEffectFilter };
